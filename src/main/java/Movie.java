1960e622a450e943b5ccb2b29b80a03a6465b5ee
dommi <dommihd@gmail.com>
2020-05-14 14:31:16 +0200

    Step Replace Conditional with Polymorphism

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -39,8 +39,6 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        return 1;
+        return priceCode.getFrequentRenterPoints(daysRented);
     }
 }
